b67702ceae26e17b72372e0453e6dc4e0f235dd7
Vladimir <vovkati3@gmail.com>
2020-01-21 20:28:41 +0300

    application editing

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,8 +2,9 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
+    flash[:notice] = "Comment add"
     redirect_to post_path(@post)
-  end
+end
    private def comment_params
      params.require(:comment).permit(:username, :body)
 end
