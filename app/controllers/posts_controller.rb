9e9673d08cb6b0b291458368405a314ef967537d
Vladimir <vovkati3@gmail.com>
2020-01-21 12:17:06 +0300

    end

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,9 +26,8 @@ class PostsController < ApplicationController
 
   def destroy
     @post = Post.find(params[:id])
-
     @post.destroy
-    redirect_to post_path
+    redirect_to posts_path
   end
 
   def create
